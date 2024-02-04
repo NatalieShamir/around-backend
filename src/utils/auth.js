2b85ffc2b2f312b0642f5d993ff76c707bd91096
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2024-02-04 11:54:52 +0200

    change base url link

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,55 +1,55 @@
-export const BASE_URL = "https://api.around.full.my.to"
+export const BASE_URL = "https://api.around.mkm.com.tr"
 
 function checkResponse(res) {
-    if (res.ok) {
-        return res.json();
-    }
-    return Promise.reject(`Error ${res.status}`);
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Error ${res.status}`);
 }
 
 export const signup = (email, password) => {
-    return fetch(`${BASE_URL}/signup`, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
+  })
+    .then(
+      checkResponse
+    )
+    .then((res) => {
+      return res;
     })
-        .then(
-            checkResponse
-        )
-        .then((res) => {
-            return res;
-        })
 }
 
 export const signin = (email, password) => {
-    return fetch(`${BASE_URL}/signin`, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
+  return fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
+  })
+    .then(checkResponse)
+    .then((data) => {
+      if (data.token) {
+        localStorage.setItem("jwt", data.token);
+        return data;
+      } else {
+        return;
+      }
     })
-        .then(checkResponse)
-        .then((data) => {
-            if (data.token) {
-                localStorage.setItem("jwt", data.token);
-                return data;
-            } else {
-                return;
-            }
-        })
 }
 
 export const checkToken = (token) => {
-    return fetch(`${BASE_URL}/users/me`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        }
-    })
-        .then(checkResponse)
-        .then(data => data)
+  return fetch(`${BASE_URL}/users/me`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    }
+  })
+    .then(checkResponse)
+    .then(data => data)
 }
